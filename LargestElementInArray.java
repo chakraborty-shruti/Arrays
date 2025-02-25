30c5b33858418ac4b54aaeb3e4a47dd81756a4c9
SHRUTI CHAKRABORTY <90564186+chakraborty-shruti@users.noreply.github.com>
2025-02-25 12:35:39 +0530

    LargestElementInArray.java

---
+++ LargestElementInArray.java
@@ -0,0 +1,30 @@
+//Largest Element in an Array
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    static int getlargest(int arr[], int n)
+    {
+        for (int i = 0; i < n; ++i) {
+            boolean flag = true;
+            for (int j = i; j < n; ++j) {
+                if (arr[j] > arr[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws IOException
+    {
+        int arr[] = { 5, 8, 20, 15 };
+        System.out.println("Largest in given array is "+getlargest(arr, 4));
+    }
+}
