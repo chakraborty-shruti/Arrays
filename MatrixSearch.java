a016d4f60bd3dff6a92c739938eab62183012aca
SHRUTI CHAKRABORTY <90564186+chakraborty-shruti@users.noreply.github.com>
2025-03-11 21:57:06 +0530

    MatrixSearch.java

---
+++ MatrixSearch.java
@@ -0,0 +1,33 @@
+//Search row-wise and column wise in a matrix
+
+class Test{
+
+    static boolean matSearch(int[][] mt,int x){
+        int m =mt.length;
+        int n = mt[0].length;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mt[i][j] == x){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] mt = {{3,45,65},
+                    {14,19,20},
+                    {43,20,87}};
+
+        int x = 19;
+
+        if(matSearch(mt,x)){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        
+    }
+}
